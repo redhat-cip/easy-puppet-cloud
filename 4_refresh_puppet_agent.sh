19efafa8735bb79cbde245c1c06d57049cc7b2d1
Nicolas Hicher <nicolas.hicher@enovance.com>
2014-03-03 21:36:07 -0500

    use local tools/ssh_config to configure hosts

--- 4_refresh_puppet_agent.sh
+++ 4_refresh_puppet_agent.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-PUPPETMASTER="192.168.134.48"
-LOGDIR="/var/www/html/log/$(date +%Y%m%d%H%M)"
+PUPPETMASTER="os-ci-test4"
+LOGDIR="log/$(date +%Y%m%d%H%M)"
+SSH_CONFIG_FILE="tools/ssh_config"
+SSH_OPTIONS="-F $SSH_CONFIG_FILE"
 echo $LOGDIR
 
-mkdir -p $LOGDIR
+if [ ! -d $LOGDIR ]; then
+  mkdir -p $LOGDIR
+fi
 
-#for i in `cat config.yaml|awk '/^ +address: / {print $2}'`; do
-#    ssh-copy-id root@${i}
-#done
-#
-while ! rsync -av --delete --exclude '*.sw?' --exclude '*~' manifests modules root@${PUPPETMASTER}:/etc/puppet; do
+hosts=$(cat config.yaml|awk '/^ +name: / {print $2}')
+
+while ! rsync -e "ssh $SSH_OPTIONS " -av --delete --exclude '*.sw?' --exclude '*~' manifests modules root@${PUPPETMASTER}:/etc/puppet; do
     sleep 1;
 done
-#
+
 cat << EOF > $LOGDIR/.htaccess
 IndexOptions FancyIndexing NameWidth=* FoldersFirst ScanHTMLTitles DescriptionWidth=*
 HeaderName HEADER.html
@@ -21,15 +23,15 @@ AllowOverride FileInfo Indexes
 Options Indexes SymLinksIfOwnerMatch
 EOF
 
-
-#for i in `cat config.yaml|awk '/^ +address: / {print $2}'`; do
-#    ssh root@${i} '
+#for host in $hosts; do
+#    ssh $SSH_OPTIONS ${host} '
 #        cp /bin/false /usr/bin/yum ; \
+#        cp /bin/false /usr/bin/apt-get ; \
 #        cp /bin/true /sbin/mkfs.xfs ; \
 #        cp /bin/true /sbin/mount.xfs ; \
 #        cp /bin/true /sbin/parted ; \
 #        service puppet stop'
-#    ssh root@${i} 'bash -c "echo nameserver 8.8.4.4 > /etc/resolv.conf"'
+#    ssh $SSH_OPTIONS ${host} 'bash -c "echo nameserver 8.8.4.4 > /etc/resolv.conf"'
 #done
 agent_pid_list=""
 #ssh root@192.168.134.49 ifconfig eth0:1 192.168.134.253
@@ -37,33 +39,33 @@ agent_pid_list=""
 echo "begin $(date)<br />" > $LOGDIR/HEADER.html
 
 
-# TODO(Gonéri)
-ssh os-ci-test7 ifconfig eth0:1 192.168.121.253
-ssh os-ci-test7 ifconfig eth1:1 192.168.134.253
-ssh os-ci-test7 ifconfig eth2:1 192.168.44.253
-
-for i in 192.168.134.49 `cat config.yaml|awk '/^ +address: / {print $2}'`; do
-    ssh root@$i 'killall -9 `cat /var/run/puppet/agent.pid`'
-    ssh root@$i '
+## TODO(Gonéri)
+#ssh os-ci-test7 ifconfig eth0:1 192.168.121.253
+#ssh os-ci-test7 ifconfig eth1:1 192.168.134.253
+#ssh os-ci-test7 ifconfig eth2:1 192.168.44.253
+#
+for host in $hosts; do
+  ssh $SSH_OPTIONS ${host} 'if [ -f /var/run/puppet/agent.pid ]; then killall -9 $(cat /var/run/puppet/agent.pid); fi'
+    ssh $SSH_OPTIONS ${host} '
        augtool -s set "/files/etc/puppet/puppet.conf/agent/server" "os-ci-test4"; \
        augtool -s set "/files/etc/puppet/puppet.conf/agent/pluginsync" "true" '
 
 
-    ssh root@$i puppet agent \
+    ssh $SSH_OPTIONS ${host} puppet agent \
         --ignorecache --waitforcert 60 \
         --no-usecacheonfailure --onetime --no-daemonize \
         --debug \
-        --server os-ci-test4 >  $LOGDIR/${i}.log 2>&1 &
+        --server $PUPPETMASTER >  $LOGDIR/${host}.log 2>&1 &
     agent_pid_list="${agent_pid_list} $!"
 done
 
 wait ${agent_pid_list}
 echo "end: <strong>$(date)</strong><br />\n" >> $LOGDIR/HEADER.html
 
-for i in `cat config.yaml|awk '/^ +address: / {print $2}'`; do
-    cat $LOGDIR/${i}.log |grep Error > /tmp/foo.log
+for host in $hosts; do
+    cat $LOGDIR/${host}.log |grep Error > /tmp/foo.log
     if [ -s "/tmp/foo.log" ]; then
-        cat /tmp/foo.log | ansi2html -p | sed -r 's,background: black;,,i' > $LOGDIR/${i}.error.html
+        cat /tmp/foo.log | ansi2html -p | sed -r 's,background: black;,,i' > $LOGDIR/${host}.error.html
     fi
-    cat $LOGDIR/${i}.log | ansi2html -p | sed -r 's,background: black;,,i' > $LOGDIR/${i}.html
+    cat $LOGDIR/${i}.log | ansi2html -p | sed -r 's,background: black;,,i' > $LOGDIR/${host}.html
 done
